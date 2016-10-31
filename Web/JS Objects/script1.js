8e0b5eb7ad7f4d6d4a930efb5108bf8967d65e21
supermeatvlad <supermeatvlad@gmail.com>
2016-10-31 22:27:26 +0200

    add js objects-1

---
+++ Web/JS Objects/script1.js
@@ -0,0 +1,11 @@
+cat = {
+	stomach: null,
+	color: 'brown',
+	name: 'Ahmed IV Salid',
+	angry: true,
+	living: ["home", "garden", "somewhere", "astral"],
+	religion: undefined,
+	age: 5,
+	weight: 9.99
+};
+console.log(cat);
